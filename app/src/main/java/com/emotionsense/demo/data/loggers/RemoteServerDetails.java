44e358bcbcde55b86e68b682c9e2aa6707b2bd27
soumya1984 <Shaurya2014$$>
2015-11-24 08:00:12 -0800

    commit

---
+++ app/src/main/java/com/emotionsense/demo/data/loggers/RemoteServerDetails.java
@@ -0,0 +1,12 @@
+package com.emotionsense.demo.data.loggers;
+
+/**
+ * Created by souachar on 11/24/15.
+ */
+public class RemoteServerDetails {
+    public static final String FILE_POST_URL = "";
+    public static final String FILE_KEY ="" ;
+    public static final String RESPONSE_ON_SUCCESS = "";
+    public static final String API_KEY_KEY = "";
+    public static final String API_KEY_VALUE = "";
+}
