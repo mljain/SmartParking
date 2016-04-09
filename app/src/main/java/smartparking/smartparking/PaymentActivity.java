cd63a5064d994d4c6802fbb035fc567ee31798e5
Mahesh Jain <mahesh.jain@apple.com>
2016-04-09 16:17:42 -0700

    Payment module with payment details and pay button.

--- app/src/main/java/smartparking/smartparking/PaymentActivity.java
+++ app/src/main/java/smartparking/smartparking/PaymentActivity.java
@@ -6,29 +6,62 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import io.card.payment.CardIOActivity;
 import io.card.payment.CreditCard;
+import smartparking.smartparking.model.User;
+import smartparking.smartparking.util.AppConstants;
 
 public class PaymentActivity extends ActionBarActivity {
 
     final String TAG = getClass().getName();
-    private Button scanButton;
+    private Button scanButton, payButton;
     private TextView resultTextView;
+    private EditText cardName, cardNumber, expiryDate, payment, CVVNumber;
     private int MY_SCAN_REQUEST_CODE = 100; // arbitrary int
-
+    private CreditCard scanResult;
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        user = (User) getIntent().getSerializableExtra(AppConstants.USER);
+
         setContentView(R.layout.activity_payment);
         resultTextView = (TextView) findViewById(R.id.resultTextView);
         scanButton = (Button) findViewById(R.id.scanButton);
+        payButton = (Button) findViewById(R.id.pay);
+        cardName = (EditText) findViewById(R.id.cardName);
+        cardNumber = (EditText) findViewById(R.id.cardNumber);
+        expiryDate = (EditText) findViewById(R.id.expiryDate);
+        CVVNumber = (EditText) findViewById(R.id.cvv);
+        payment = (EditText) findViewById(R.id.amount);
 
         resultTextView.setText("card.io library version: " + CardIOActivity.sdkVersion() + "\nBuilt: " + CardIOActivity.sdkBuildDate());
 
+        payButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String resultStr;
+                if(scanResult != null) {
+                    resultStr = "Payment Successfull. Thank you.";
+                    resultTextView.setText(resultStr);
+                    Toast.makeText(PaymentActivity.this, resultStr, Toast.LENGTH_LONG).show();
+                    Intent findParkingActivity = new Intent(PaymentActivity.this, FirstScreen.class);
+                    findParkingActivity.putExtra(AppConstants.USER, user);
+                    startActivity(findParkingActivity);
+                }else{
+                    resultStr = "Please provide payment details.";
+                    resultTextView.setText(resultStr);
+                    Toast.makeText(PaymentActivity.this, resultStr, Toast.LENGTH_LONG).show();
+                    resultTextView.setText(resultStr);
+                }
+
+            }
+        });
     }
 
     @Override
@@ -87,21 +120,24 @@ public class PaymentActivity extends ActionBarActivity {
 
         String resultStr;
         if (data != null && data.hasExtra(CardIOActivity.EXTRA_SCAN_RESULT)) {
-            CreditCard scanResult = data.getParcelableExtra(CardIOActivity.EXTRA_SCAN_RESULT);
+            scanResult = data.getParcelableExtra(CardIOActivity.EXTRA_SCAN_RESULT);
 
             // Never log a raw card number. Avoid displaying it, but if necessary use getFormattedCardNumber()
             resultStr = "Card Number: " + scanResult.getRedactedCardNumber() + "\n";
+            cardNumber.setText(scanResult.getRedactedCardNumber());
 
             // Do something with the raw number, e.g.:
             // myService.setCardNumber( scanResult.cardNumber );
 
             if (scanResult.isExpiryValid()) {
                 resultStr += "Expiration Date: " + scanResult.expiryMonth + "/" + scanResult.expiryYear + "\n";
+                expiryDate.setText(scanResult.expiryMonth + "/" + scanResult.expiryYear);
             }
 
             if (scanResult.cvv != null) {
                 // Never log or display a CVV
                 resultStr += "CVV has " + scanResult.cvv.length() + " digits.\n";
+                CVVNumber.setText(scanResult.cvv);
             }
 
             if (scanResult.postalCode != null) {
@@ -110,6 +146,7 @@ public class PaymentActivity extends ActionBarActivity {
 
             if (scanResult.cardholderName != null) {
                 resultStr += "Cardholder Name : " + scanResult.cardholderName + "\n";
+                cardName.setText(scanResult.cardholderName);
             }
         } else {
             resultStr = "Scan was canceled.";
